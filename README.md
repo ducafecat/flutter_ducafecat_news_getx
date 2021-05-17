ebfacf3eda3214ee4fbc92bc43f4780c1b7d87dd
ducafecat <ducafecat@gmail.com>
2021-05-17 16:09:13 +0800

    Add -> 初始

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_ducafecat_news_getx
+
+新闻客户端 Getx 版本
---
+++ android/app/src/main/kotlin/com/example/flutter_ducafecat_news_getx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ducafecat_news_getx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
